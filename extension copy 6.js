8dee7778344832bc667bb49f88efc3885c7850ff
faymaz <fethi.aymaz@gmail.com>
2024-10-27 12:53:42 +0100

    Last changes

--- extension copy 6.js
+++ extension copy 6.js
@@ -36,7 +36,7 @@ const PrayerTimesIndicator = GObject.registerClass(
       this._extension = extension;
 
       this.icon = new St.Icon({
-        icon_name: 'system-run', // Geçici olarak varsayılan ikon
+        gicon: Gio.icon_new_for_string(`${extension.path}/icons/icon.svg`),
         style_class: 'system-status-icon',
       });
       this.add_child(this.icon);
@@ -52,7 +52,11 @@ const PrayerTimesIndicator = GObject.registerClass(
 
       // Şehir verilerini doğrudan burada tanımlıyoruz
       this.cities = [
-        // Şehir listesi
+        { name: 'Mörfelden-Walldorf', url: 'https://namazvakitleri.diyanet.gov.tr/en-US/10214/morfelden---walldorf-icin-namaz-vakti' },
+        { name: 'Tuttlingen', url: 'https://namazvakitleri.diyanet.gov.tr/en-US/11083/tuttlingen-icin-namaz-vakti' },
+        { name: 'Costa Mesa', url: 'https://namazvakitleri.diyanet.gov.tr/en-US/8608/costa-mesa-icin-namaz-vakti' },
+        { name: 'Irvine', url: 'https://namazvakitleri.diyanet.gov.tr/en-US/8622/irvine-icin-namaz-vakti' },
+        { name: 'Clifton', url: 'https://namazvakitleri.diyanet.gov.tr/en-US/8869/clifton-icin-namaz-vakti' },
       ];
 
       // Ayar değişikliklerini dinliyoruz
@@ -76,30 +80,88 @@ const PrayerTimesIndicator = GObject.registerClass(
       let city = this.cities.find((c) => c.name === selectedCityName);
       log('Namaz vakitleri güncelleniyor...');
       if (!city) {
+        // Eğer şehir bulunamazsa varsayılan olarak ilk şehri kullan
         city = this.cities[0];
         this.settings.set_string('selected-city', city.name);
         log('Namaz vakitleri güncellendi.');
       }
 
-      let session = Soup.Session.new();
-
+      let session = new Soup.Session();
       let message = new Soup.Message('GET', city.url);
 
-      session.send_and_read_async(message)
-        .then((bytes) => {
+      session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (session, result) => {
+        try {
+          let bytes = session.send_and_read_finish(result);
           if (message.status_code !== Soup.Status.OK) {
             throw new Error(`HTTP isteği başarısız oldu: ${message.status_code}`);
           }
           let decoder = new TextDecoder('utf-8');
           let html = decoder.decode(bytes.get_data());
-          this._parsePrayerTimes(html);
-        })
-        .catch((e) => {
+          GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+            this._parsePrayerTimes(html);
+            return GLib.SOURCE_REMOVE;
+          });
+        } catch (e) {
           logError(e);
-        });
+        }
+      });
     }
 
-    // Diğer metotlar aynı kalabilir
+    _parsePrayerTimes(html) {
+      // Namaz vakitlerini çıkarmak için düzenli ifade
+      let regex = /<div\s+class="tpt-cell"\s+data-vakit-name="(imsak|gunes|ogle|ikindi|aksam|yatsi)".*?<div\s+class="tpt-time">(\d{2}:\d{2})<\/div>/gs;
+      let matches = [...html.matchAll(regex)];
+
+      if (matches.length < 6) {
+        log('Tüm namaz vakitleri bulunamadı. Elde edilen matches.length: ' + matches.length);
+        return;
+      }
+
+      let times = {};
+      for (let match of matches) {
+        let vakitName = match[1];
+        let timeString = match[2];
+        times[vakitName] = this._parseTime(timeString);
+      }
+
+      let now = new Date();
+
+      // Namaz vakitlerini kontrol edip sonraki vakti bulalım
+      let vakitler = ['imsak', 'gunes', 'ogle', 'ikindi', 'aksam', 'yatsi'];
+      let nextPrayer = null;
+
+      for (let i = 0; i < vakitler.length; i++) {
+        let vakit = vakitler[i];
+        if (now < times[vakit]) {
+          nextPrayer = { name: vakit, time: times[vakit] };
+          break;
+        }
+      }
+
+      if (!nextPrayer) {
+        // Ertesi günün imsak vaktini alalım
+        nextPrayer = { name: 'imsak', time: this._addDays(times['imsak'], 1) };
+      }
+
+      let timeLeft = Math.floor((nextPrayer.time - now) / 60000); // Dakika cinsinden
+
+      this.label.set_text(`${nextPrayer.name}: ${timeLeft} dk`);
+
+      // Burada bildirim ve diğer işlemleri yapabilirsiniz
+    }
+
+    _parseTime(timeString) {
+      let [hours, minutes] = timeString.split(':').map(Number);
+      let date = new Date();
+      date.setHours(hours, minutes, 0, 0);
+      return date;
+    }
+
+    _addDays(date, days) {
+      let newDate = new Date(date);
+      newDate.setDate(newDate.getDate() + days);
+      return newDate;
+    }
 
     destroy() {
       if (this._timer) {
